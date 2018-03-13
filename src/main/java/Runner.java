852f3741aa9f8e8f63ed64e85af930ebd5d02807
Angus Malloch <angusbass@hotmail.com>
2018-03-13 20:31:57 +0000

    Problem solved! A capitol M in models folder.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,14 +1,16 @@
-import Models.File;
-import Models.Folder;
+import models.File;
+import models.Folder;
 import db.DBHelper;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        File file1 = new File();
         Folder folder1 = new Folder("Thee Folder");
+        File file1 = new File("secret", "jpeg", 20, folder1);
         DBHelper.save(file1);
+
+//        String name, String extension, int size, Folder folder
 
     }
 }
