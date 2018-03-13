8edbef280f513b209bb2d70aad67ed70ba1463ff
Angus Malloch <angusbass@hotmail.com>
2018-03-13 21:32:15 +0000

    get all fixed added delete method

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,11 +9,17 @@ public class Runner {
     public static void main(String[] args) {
 
         Folder folder1 = new Folder("Thee Folder");
+        DBHelper.save(folder1);
         File file1 = new File("secret", "jpeg", 20, folder1);
         DBHelper.save(file1);
 
         File file2 = new File("happy", "png", 30, folder1);
         DBHelper.save(file2);
+
+        File file3 = new File("sad", "png", 15, folder1);
+        DBHelper.save(file3);
+
+        DBHelper.delete(file3);
 
         List<File> fileList = DBHelper.getAll(File.class);
 
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -63,4 +63,18 @@ public class DBHelper {
         return results;
     }
 
+    public static void delete(Object object) {
+        session = HibernateUtil.getSessionFactory().openSession();
+        try{
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
 }
